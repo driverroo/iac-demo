cef36c9d6ce972c2236217b8b93ce763003209b2
driverroo <53400158+driverroo@users.noreply.github.com>
2020-11-23 20:12:15 -0500

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  type    = string
+  default = "us-west-2"
+
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  type    = list
+  default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+}
+
+
+variable "azs" {
+  type    = list
+  default = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,19 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+resource "aws_vpc" "tf-vpc" {
+  cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "default"
 
   tags = {
     Name = "tf-vpc"
-    Purpose = "Jenkins Demo"
+    Purpose = "DevOps TF Build"
+  }
+}
+resource "aws_subnet" "subnets" {
+  count = "${length(var.azs)}"
+
+vpc_id     = "${aws_vpc.tf-vpc.id}"
+  cidr_block = "${element(var.subnet_cidr,count.index)}"
+
+  tags = {
+    Name = "sub_az1-${count.index+1}"
   }
 }
